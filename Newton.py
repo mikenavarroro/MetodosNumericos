1d51bfe638870beae6fb0e8000a2333792e62108
mikenavarroro <mike.navarroro@gmail.com>
2020-01-22 04:44:54 -0600

    Interfaz con gráfica

--- Newton.py
+++ Newton.py
@@ -5,36 +5,94 @@ TW: @MikeNavarroR
 e-mail: mike.navarroro@gmail.com
 
 '''
-import msvcrt as mv
-from math import e, pi
-from math import *
-from sympy import *
+
 import sympy as sp
+import tkinter as tk
+from numpy import *
+from matplotlib import pyplot as plt
+
+ven = tk.Tk()
+ven.geometry('600x500')
+ven.title("Newton")
+ven.resizable(False, False)
+
+funcion = tk.StringVar()
+a_var = tk.StringVar()
+err_u = tk.StringVar()
 
 x = sp.symbols('x')
-funcion = input("Escribe la función en términos de x: ")
-ini = float(input("Valor inicial: "))
-erroru = float(input("Error: "))
-func=sp.sympify(funcion)
-derivada= func.diff(x)
-error = 1
-lista = []
-i = 0
-print("{:>11} {:>11} {:>11} {:>11}".format('Xn', 'f(Xn)', "f'(Xn)", 'Error'))
 
-while error > erroru:
-    fini = sp.sympify(funcion).subs(x, ini)
-    fder = sp.sympify(derivada).subs(x, ini)
-    x1 = ini - (fini/fder)
-    if i == 0:
-        lista.append(ini)
-    lista.append(x1)
-    if i != 0:
-        error = abs((lista[i]-lista[i-1])/lista[i])
-    print("{:<2}| {:^10.10s}| {:^10.10s}| {:^10.10s}| {:^10.10s}|".format(str(i+1),str(ini),str(fini),str(fder),str(error)))
-    print("-----------------------------------------------------")
-    ini = x1
-    i += 1
-mv.getch()
-print("Presiona cualquier tecla para salir")
-mv.getch()
+lblfun = tk.Label(ven, text="Función:").grid(row = 0, column = 1)
+lblfunc = tk.Label(ven, text="f(x)").grid(row = 1, column = 5)
+entfunc = tk.Entry(ven, textvariable=funcion).grid(row = 2, column = 5)
+lbla = tk.Label(ven, text="Valor inicial").grid(row = 4, column = 5)
+enta = tk.Entry(ven, textvariable=a_var).grid(row = 5, column = 5)
+lblerr = tk.Label(ven, text="Error:").grid(row = 8, column = 1)
+lblerroru = tk.Label(ven, text="error").grid(row = 9, column = 5)
+enterroru = tk.Entry(ven, textvariable=err_u).grid(row = 10, column = 5)
+
+def iniciar():
+    vent = tk.Toplevel(ven)
+
+    funci = funcion.get()
+    erroru = float(err_u.get())
+    ini = float(a_var.get())
+
+    func = sp.sympify(funci)
+    derivada = func.diff(x)
+    error = 1
+    lista = []
+    i = 0
+
+    vent.title(funci)
+
+    m = tk.Label(vent, text="n", bg = "black", fg = "white", width=20, height=1).grid(row = 0, column = 0)
+    m = tk.Label(vent, text="Xn", bg = "black", fg = "white", width=20, height=1).grid(row = 0, column = 1)
+    m = tk.Label(vent, text="f(Xn)", bg = "black", fg = "white", width=30, height=1).grid(row = 0, column = 2)
+    m = tk.Label(vent, text="f'(Xn)", bg = "black", fg = "white", width=30, height=1).grid(row = 0, column = 3)
+    m = tk.Label(vent, text="error", bg = "black", fg = "white", width=20, height=1).grid(row = 0, column = 4)
+
+    while error > erroru:
+        fini = sp.sympify(funci).subs(x, ini)
+        fder = sp.sympify(derivada).subs(x, ini)
+        x1 = ini - (fini/fder)
+        if i == 0:
+            lista.append(ini)
+        lista.append(x1)
+        if i != 0:
+            error = abs((lista[i]-lista[i-1])/lista[i])
+        m = tk.Label(vent, text=i+1, width=10, height=1).grid(row = i+1, column = 0)
+        m = tk.Label(vent, text=ini).grid(row = i+1, column = 1)
+        m = tk.Label(vent, text=fini, width=20, height=1).grid(row = i+1, column = 2)
+        m = tk.Label(vent, text=fder, width=20, height=1).grid(row = i+1, column = 3)
+        m = tk.Label(vent, text=error).grid(row = i+1, column = 4)
+        ini = x1
+        i += 1
+
+
+def graficar():
+    funci = funcion.get()
+    func = sp.sympify(funci)
+    derivada = func.diff(x)
+
+    a, b, z, y = [], [], [], []
+
+    for i in range(-100, 101):
+        z.append(i)
+        a.append(i)
+        y.append(sp.sympify(func).subs(x, i))
+        b.append(sp.sympify(derivada).subs(x, i))
+
+
+    plt.plot(z, y)
+    plt.plot(a, b)
+    plt.title(func)
+    plt.axhline(0, color="black")
+    plt.axvline(0, color="black")
+    plt.show()
+
+
+tabular = tk.Button(ven, text="Tabular", command=iniciar).grid(row = 11, column = 3)
+graficar = tk.Button(ven, text="Graficar", command=graficar).grid(row = 11, column = 5)
+
+ven.mainloop()
--- PivoteoTotal.py
+++ PivoteoTotal.py
@@ -6,13 +6,32 @@ e-mail: mike.navarroro@gmail.com
 
 '''
 from math import *
-import msvcrt as mv
 from numpy import *
 import numpy as np
+import tkinter as tk
 
-print("Matriz A")
-dim = int(input("Dimensión de la matriz de nxn: "))
-A = np.empty((dim, dim))
+ven = tk.Tk()
+ven.geometry('600x500')
+dim_var = tk.IntVar()
+mat = tk.StringVar()
+
+lblA = tk.Label(ven, text="Matriz A").grid(row = 0, column = 0)
+lbldim = tk.Label(ven, text="Dimensión de la matriz de nxn").grid(row = 1, column = 0)
+entdim = tk.Entry(ven, textvariable=dim_var).grid(row = 1, column = 1)
+def aceptar():
+    dim = dim_var.get()
+    A = np.empty((dim, dim))
+    for i in range(dim):
+        for j in range(dim):
+            m = tk.Entry(ven, width= 10, textvariable=mat).place(x = i*70+100, y = j*50+70)
+            lista[j] = mat.get()
+    for i in range(dim):
+        for j in range(dim):
+            A[i, j] = lista[i, j]
+    print(A)
+
+btnaceptar = tk.Button(ven, text="Aceptar", command=aceptar).grid(row = 0, column = 1)
+'''
 for i in range(dim):
     for j in range(dim):
         A[i, j] = float(input("Dame el número en la posición {}, {}: ".format(i+1, j+1)))
@@ -27,6 +46,5 @@ print("="*dim)
 print(B)
 print("="*dim)
 print(solucion)
-mv.getch()
-print("Presiona cualquier tecla para salir")
-mv.getch()
+'''
+ven.mainloop()
